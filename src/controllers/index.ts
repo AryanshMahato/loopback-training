d614539db84df521fe2a6e4eed6788e6fd75bf26
Aryansh Mahato <aryansh.mahato@sourcefuse.com>
2023-03-08 18:44:46 +0530

    add proper relations

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,7 @@
 export * from './ping.controller';
 export * from './user.controller';
 export * from './customers.controller';
+export * from './user-customer.controller';
+export * from './user-role.controller';
+export * from './role-user.controller';
+export * from './customer-user.controller';
--- src/repositories/role.repository.ts
+++ src/repositories/role.repository.ts
@@ -1,16 +1,22 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
 import {CustomerDataSource} from '../datasources';
-import {Role, RoleRelations} from '../models';
+import {Role, RoleRelations, User} from '../models';
+import {UserRepository} from './user.repository';
 
 export class RoleRepository extends DefaultCrudRepository<
   Role,
   typeof Role.prototype.id,
   RoleRelations
 > {
+
+  public readonly user: BelongsToAccessor<User, typeof Role.prototype.id>;
+
   constructor(
-    @inject('datasources.Customer') dataSource: CustomerDataSource,
+    @inject('datasources.Customer') dataSource: CustomerDataSource, @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>,
   ) {
     super(Role, dataSource);
+    this.user = this.createBelongsToAccessorFor('user', userRepositoryGetter,);
+    this.registerInclusionResolver('user', this.user.inclusionResolver);
   }
 }
