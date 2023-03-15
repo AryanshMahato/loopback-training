5311643489f0f35b9b9219b8402ec421ea1a4b63
Aryansh Mahato <aryansh.mahato@sourcefuse.com>
2023-03-15 14:35:00 +0530

    add wildcard operator in cors middleware

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -2,10 +2,24 @@ import {Middleware} from '@loopback/rest';
 
 export const corsMiddleware: Middleware = async (context, next) => {
   const {headers} = context.request;
-  const allowedOrigins = process.env.ALLOWED_ORIGINS ?? '';
+  const allowedOrigins = process.env.ALLOWED_ORIGINS;
+  if (allowedOrigins === '*') {
+    return next();
+  }
 
-  if (!headers['referer'] || !allowedOrigins.includes(headers['referer'])) {
-    context.response.status(403).json({error: 'Referer did not matched'});
+  const referer = headers['referer'];
+  if (!referer) {
+    return context.response.status(403).json({error: 'Referer not found'});
+  }
+
+  if (!allowedOrigins) {
+    return context.response.status(500).json({error: 'Referer not configured'});
+  }
+
+  if (!allowedOrigins.includes(referer)) {
+    return context.response
+      .status(403)
+      .json({error: 'Referer did not matched'});
   }
 
   return next();
